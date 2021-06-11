333d3416f158a00ced6b0901b1e2b81e7557404f
smoothyfinance <github@smoothy.finance>
2021-06-11 19:27:01 +0800

    apy with ypool

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,10 @@
-import { log } from '@graphprotocol/graph-ts'
+import { BigInt, Address, log } from '@graphprotocol/graph-ts'
 import { Contract, Transfer } from "../generated/Contract/Contract"
+import { YToken } from "../generated/Contract/YToken"
 import { SyUSD } from "../generated/schema"
+
+let DEFAULT_DECIMALS = BigInt.fromI32(10).pow(18)
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 export function handleTransfer(event: Transfer): void {
   let id = event.block.hash.toHex()
@@ -21,7 +25,61 @@ export function handleTransfer(event: Transfer): void {
   if (tb.reverted) {
     log.info("totalBalance reverted", [])
   } else {
-    entity.totalBalance = tb.value
+    let yb = getYBalance(contract, event.address)
+    log.debug("totalBalance = {}, yBalanceToAdd = {}", [tb.value.toString(), yb.toString()])
+    entity.totalBalance = tb.value.plus(yb)
   }
   entity.save()
+}
+
+function getYBalance(contract: Contract, address: Address): BigInt {
+  let token_len = contract.try__ntokens()
+  if (token_len.reverted) {
+    return BigInt.fromI32(0)
+  }
+  let yBalance_total = BigInt.fromI32(0)
+  for (let i = BigInt.fromI32(0); i.lt(token_len.value); i = i.plus(BigInt.fromI32(1))) {
+    let addr = contract.try__yTokenAddresses(i)
+    if (addr.reverted) {
+      log.debug("try__yTokenAddresses revert", [])
+      continue
+    }
+    if (addr.value.toHexString() == ZERO_ADDRESS) {
+      // log.debug("_yTokenAddresses is empty", [])
+      continue
+    }
+    let yToken = YToken.bind(addr.value)
+    let symbol = yToken.try_symbol()
+    if (symbol.reverted) {
+      log.debug("try_symbol revert: address={}", [addr.value.toHexString()])
+      continue
+    }
+    let pricePerShare = yToken.try_getPricePerFullShare()
+    if (pricePerShare.reverted) {
+      log.debug("try_getPricePerFullShare revert", [])
+      continue
+    }
+    let share = yToken.try_balanceOf(address)
+    if (share.reverted) {
+      log.debug("try_balanceOf revert", [])
+      continue
+    }
+    let decm = yToken.try_decimals()
+    if (decm.reverted) {
+      log.debug("try_decimals revert", [])
+      continue
+    }
+    log.debug("i={} | yTokenAddress={} | symbol={} | decimal={}", [i.toString(), addr.value.toHexString(), symbol.value, BigInt.fromI32(decm.value).toString()])
+    let yBalanceOldNormalized = contract.try__yBalances(i)
+    if (yBalanceOldNormalized.reverted) {
+      log.debug("try__yBalances revert", [])
+      continue
+    }
+    let yBalanceNewUnnormalized = share.value.times(pricePerShare.value).div(DEFAULT_DECIMALS)
+    let yBalanceNewNormalized = yBalanceNewUnnormalized.times(BigInt.fromI32(10).pow(<u8>(18 - decm.value)))
+    let yBalance = yBalanceNewNormalized.minus(yBalanceOldNormalized.value)
+    yBalance_total = yBalance_total.plus(yBalance)
+    log.debug("i={} |{} - {}| yBalance={} | yBalance_total={}", [i.toString(), yBalanceNewNormalized.toString(), yBalanceOldNormalized.value.toString(), yBalance.toString(), yBalance_total.toString()])
+  }
+  return yBalance_total
 }
